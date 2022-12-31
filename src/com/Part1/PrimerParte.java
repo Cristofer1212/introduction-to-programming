25bd8463655d35fbb4b79016ea7d9f0561f7674b
Cristofer Azaña <cristoferprograma@gmail.com>
2022-12-30 22:45:53 -0500

    first commit

---
+++ src/com/Part1/PrimerParte.java
@@ -0,0 +1,12 @@
+package com.Part1;
+
+public class PrimerParte {
+    public static void main(String[] args) {
+        suma(18, 12, 12);
+    }
+
+    public static void suma(int num1 , int num2, int num3) {
+        int suma = num1 + num2 + num3;
+        System.out.println(suma);
+    }
+}
